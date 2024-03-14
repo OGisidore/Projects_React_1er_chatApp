d01be66e110ab8cb8717134e2c57d15b154fd616
OGisidore <isidoreogoulodo43@gmail.com>
2024-03-15 00:41:11 +0100

    s commit

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,17 @@
+import { Discussion } from "./Discussions"
+import { RECEIVE, SEND } from "./action/actions"
+
 export interface User{
-    name: string
+    name: string,
     email: string,
     tel: number,
     onLine:boolean,
-    infos :string
+    infos :string,
     profilImage: string,
-    message : string
+    message : {
+        time:string,
+        content:string
+    }
+    discussions: Discussion[]
 
 }
